760ddb724ba0f83bb21542cba89bd646c7ace708
clnmlg <colin.mlg@posteo.net>
2024-05-25 18:06:51 +0200

    quick fixes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
-module.exports = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'imgur.com',
-            },
-        ],
+export const images = {
+  unoptimized: true,
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "res.cloudinary.com",
     },
-}
+    {
+      protocol: "https",
+      hostname: "avatars.githubusercontent.com",
+    },
+    {
+      protocol: "https",
+      hostname: "imgur.com",
+    },
+  ],
+};
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,38 +1,36 @@
-import styles from '../styles/HomePage.module.css'
-import Link from 'next/link'
+import styles from "../styles/HomePage.module.css";
+import Link from "next/link";
 
 const AboutPage = () => {
-    return (
-        <div>
-            <h3>A Little Bit About Me</h3>
-            <br />
-            <h5 className={styles.textWeight}>
-                Hello there! I'm Colin, a Broadcast Audiovisual Technician at
-                WarnerBros with a deep passion for development. Before joining
-                WarnerBros, I immersed myself in the world of programming,
-                focusing particularly on front-end development. My favorite
-                technologies include React, NextJS, and TypeScript, among many
-                others. On this portfolio, you'll find a showcase of my
-                projects, skills, and the journey that has brought me to where I
-                am today. Feel free to explore and get in touch if you'd like to
-                collaborate or learn more about my work. Thank you for visiting
-                _
-            </h5>
-            <br />
-            <Link href="/projects">
-                <button className={styles.button}>View Work</button>
-            </Link>
-            <Link href="/contact">
-                <button className={styles.outlined}>Contact Me</button>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3>A Little Bit About Me</h3>
+      <br />
+      <h5 className={styles.textWeight}>
+        Hello there! I'm Colin, a Broadcast Audiovisual Technician at WarnerBros
+        with a deep passion for development. Before joining WarnerBros, I
+        immersed myself in the world of programming, focusing particularly on
+        front-end development. My favorite technologies include React, NextJS,
+        and TypeScript, among many others. On this portfolio, you'll find a
+        showcase of my projects, skills, and the journey that has brought me to
+        where I am today. Feel free to explore and get in touch if you'd like to
+        collaborate or learn more about my work. Thank you for visiting _
+      </h5>
+      <br />
+      <Link href="/projects" preload="true">
+        <button className={styles.button}>View Work</button>
+      </Link>
+      <Link href="/contact" preload="true">
+        <button className={styles.outlined}>Contact Me</button>
+      </Link>
+    </div>
+  );
+};
 
 export async function getStaticProps() {
-    return {
-        props: { title: 'About' },
-    }
+  return {
+    props: { title: "About" },
+  };
 }
 
-export default AboutPage
+export default AboutPage;
